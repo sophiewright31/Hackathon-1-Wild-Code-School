3f74d41fa8889c5d37050aff0ac7a1061beade6a
Sara Ajana <sara.ajana@kedgebs.com>
2021-05-11 19:02:58 +0200

    map

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -1,13 +1,11 @@
 <?php
 
-
 namespace App\Controller;
-
 
 class GameController extends AbstractController
 {
     public function index()
     {
         return $this->twig->render('Game/index.html.twig');
-  }
+    }
 }
